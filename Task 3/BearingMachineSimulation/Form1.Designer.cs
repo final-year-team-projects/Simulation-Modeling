4df010cf235d36d220a7a5e3db12a4695be2db32
muhammed3ly <40716171+muhammed3ly@users.noreply.github.com>
2019-12-03 00:26:00 +0200

    Task 3

---
+++ Task 3/BearingMachineSimulation/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace BearingMachineSimulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Testing File";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(199, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(206, 67);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Evaluate The System";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(688, 79);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "The Milling Machine System";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 104);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1093, 512);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1085, 483);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Current Method";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1079, 477);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.splitContainer1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1085, 483);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Proposed Method";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(1079, 276);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 62.72563F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.27437F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1108, 79);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 48.28431F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 51.71569F));
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(697, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(408, 73);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView3);
+            this.splitContainer1.Size = new System.Drawing.Size(1079, 477);
+            this.splitContainer1.SplitterDistance = 276;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView3.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView3.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(1079, 197);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1108, 617);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "The Milling Machine System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+    }
+}
---
+++ Task 3/BearingMachineSimulation/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BearingMachineTesting;
+using BearingMachineModels;
+using System.IO;
+
+namespace BearingMachineSimulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SimulationSystem simulationSystem = new SimulationSystem();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string testingResult = TestingManager.Test(simulationSystem, Constants.FileNames.TestCase1);
+            MessageBox.Show(testingResult);
+        }
+        
+        int binarySearch(int rnd, List<TimeDistribution> lst)
+        {
+            int l = 0, r = lst.Count - 1, mid;
+            while (l <= r)
+            {
+                mid = (l + r) / 2;
+                if (lst[mid].MinRange <= rnd && lst[mid].MaxRange >= rnd)
+                    return lst[mid].Time;
+                else if (lst[mid].MinRange < rnd && lst[mid].MaxRange < rnd)
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
+            return 0;
+        }
+        
+        void TakeInput()
+        {
+            string[] lines = File.ReadAllLines(openFileDialog1.FileName);
+            simulationSystem.DowntimeCost = Convert.ToInt32(lines[1]);
+            simulationSystem.RepairPersonCost = Convert.ToInt32(lines[4]);
+            simulationSystem.BearingCost = Convert.ToInt32(lines[7]);
+            simulationSystem.NumberOfHours = Convert.ToInt32(lines[10]);
+            simulationSystem.NumberOfBearings = Convert.ToInt32(lines[13]);
+            simulationSystem.RepairTimeForOneBearing = Convert.ToInt32(lines[16]);
+            simulationSystem.RepairTimeForAllBearings = Convert.ToInt32(lines[19]);
+            int i = 22;
+            decimal lst = 0;
+            for (; lines[i] != ""; i++)
+            {
+                string[] separator = { ", " };
+                string[] values = lines[i].Split(separator, 2, StringSplitOptions.RemoveEmptyEntries);
+                TimeDistribution td = new TimeDistribution();
+                td.Time = Convert.ToInt32(values[0]);
+                td.Probability = Convert.ToDecimal(values[1]);
+                td.CummProbability = lst + td.Probability;
+                td.MinRange = Convert.ToInt32(lst * 100) + 1;
+                lst = td.CummProbability;
+                td.MaxRange = Convert.ToInt32(lst * 100);
+                simulationSystem.DelayTimeDistribution.Add(td);
+            }
+            i += 2;
+            lst = 0;
+            for (; i < lines.Length; i++)
+            {
+                string[] separator = { ", " };
+                string[] values = lines[i].Split(separator, 2, StringSplitOptions.RemoveEmptyEntries);
+                TimeDistribution td = new TimeDistribution();
+                td.Time = Convert.ToInt32(values[0]);
+                td.Probability = Convert.ToDecimal(values[1]);
+                td.CummProbability = lst + td.Probability;
+                td.MinRange = Convert.ToInt32(lst * 100) + 1;
+                lst = td.CummProbability;
+                td.MaxRange = Convert.ToInt32(lst * 100);
+                simulationSystem.BearingLifeDistribution.Add(td);
+            }
+        }
+        void BuildTable1()
+        {
+            int delayt = 0;
+
+            DataTable dc = new DataTable(), dc2 = new DataTable(), dc3 = new DataTable();
+            dc.Columns.Add("Bearing Index");
+            dc2.Columns.Add("Bearing rnd");
+            dc3.Columns.Add("bearing life");
+            Random rnd = new Random();
+            for (int bearingIDX = 1; bearingIDX <= simulationSystem.NumberOfBearings; bearingIDX++)
+            {
+                for (int life = 0; life < simulationSystem.NumberOfHours;)
+                {
+                    CurrentSimulationCase csc = new CurrentSimulationCase();
+                    csc.Bearing.Index = bearingIDX;
+                    dc.Rows.Add(bearingIDX);
+                    csc.Bearing.RandomHours= rnd.Next(1, 100);
+                    dc2.Rows.Add(csc.Bearing.RandomHours);
+                    csc.Bearing.Hours = binarySearch(csc.Bearing.RandomHours, simulationSystem.BearingLifeDistribution);
+                    dc3.Rows.Add(csc.Bearing.Hours);
+                    int lstIdx = simulationSystem.CurrentSimulationTable.Count;
+                    csc.RandomDelay = rnd.Next(1, 100);
+                    csc.AccumulatedHours = csc.Bearing.Hours + life;
+                    csc.Delay = binarySearch(csc.RandomDelay, simulationSystem.DelayTimeDistribution);
+                    simulationSystem.CurrentSimulationTable.Add(csc);
+                    delayt += csc.Delay;
+                    life = csc.AccumulatedHours;
+                }
+            }
+            simulationSystem.CurrentPerformanceMeasures.BearingCost = simulationSystem.CurrentSimulationTable.Count*simulationSystem.BearingCost;
+            simulationSystem.CurrentPerformanceMeasures.DelayCost = delayt * simulationSystem.DowntimeCost;
+            simulationSystem.CurrentPerformanceMeasures.DowntimeCost = simulationSystem.CurrentSimulationTable.Count * simulationSystem.RepairTimeForOneBearing* simulationSystem.DowntimeCost;
+            simulationSystem.CurrentPerformanceMeasures.RepairPersonCost = (decimal)simulationSystem.CurrentSimulationTable.Count * simulationSystem.RepairTimeForOneBearing * simulationSystem.RepairPersonCost / 60;
+
+            simulationSystem.CurrentPerformanceMeasures.TotalCost = simulationSystem.CurrentPerformanceMeasures.BearingCost + simulationSystem.CurrentPerformanceMeasures.DelayCost + simulationSystem.CurrentPerformanceMeasures.DowntimeCost + simulationSystem.CurrentPerformanceMeasures.RepairPersonCost;
+            dataGridView1.DataSource = simulationSystem.CurrentSimulationTable;
+            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.Columns.Add("bearingIDX", "bearingIDX");
+            dataGridView1.Columns.Add("bearingRND", "bearingRND");
+            dataGridView1.Columns.Add("bearingLife", "bearingLife");
+            dataGridView1.Columns["bearingIDX"].DisplayIndex = 0;
+            dataGridView1.Columns["bearingRND"].DisplayIndex = 1;
+            dataGridView1.Columns["bearingLife"].DisplayIndex = 2;
+            for (int i = 0; i < simulationSystem.CurrentSimulationTable.Count; i++)
+            {
+                dataGridView1[dataGridView1.Columns.Count - 3, i].Value = Convert.ToInt32(dc.Rows[i][0]);
+                dataGridView1[dataGridView1.Columns.Count - 2, i].Value = Convert.ToInt32(dc2.Rows[i][0]);
+                dataGridView1[dataGridView1.Columns.Count - 1, i].Value = Convert.ToInt32(dc3.Rows[i][0]);
+            }
+        }
+        void BuildTable2()
+        {
+            List<List<Bearing>> bearingsCategorized = new List<List<Bearing>>();
+            int bearingIDX = 0;
+            foreach(CurrentSimulationCase csc in simulationSystem.CurrentSimulationTable)
+            {
+                Bearing b = csc.Bearing;
+                if (b.Index != bearingIDX)
+                {
+                    bearingsCategorized.Add(new List<Bearing>());
+                    bearingIDX++;
+                }
+                bearingsCategorized[bearingsCategorized.Count - 1].Add(b);
+            }
+
+            int delayt = 0;
+            Random rnd = new Random();
+            for (int life = 0, j = 0; life < simulationSystem.NumberOfHours; j++)
+            {
+                ProposedSimulationCase psc = new ProposedSimulationCase();
+                psc.RandomDelay = rnd.Next(1, 100);
+                psc.Delay = binarySearch(psc.RandomDelay, simulationSystem.DelayTimeDistribution);
+                delayt += psc.Delay;
+                psc.AccumulatedHours = life;
+                psc.FirstFailure = int.MaxValue;
+                for (bearingIDX = 0; bearingIDX < simulationSystem.NumberOfBearings; bearingIDX++)
+                {
+                    if(bearingsCategorized[bearingIDX].Count <= j)
+                    {
+                        Bearing b = new Bearing();
+                        b.Index = bearingIDX + 1;
+                        b.RandomHours = rnd.Next(1, 100);
+                        b.Hours = binarySearch(b.RandomHours, simulationSystem.BearingLifeDistribution);
+                        psc.Bearings.Add(b);
+                        psc.FirstFailure = Math.Min(b.Hours, psc.FirstFailure);
+                    }
+                    else
+                    {
+                        psc.Bearings.Add(bearingsCategorized[bearingIDX][j]);
+                        psc.FirstFailure = Math.Min(bearingsCategorized[bearingIDX][j].Hours, psc.FirstFailure);
+                    }
+                }
+                psc.AccumulatedHours += psc.FirstFailure;
+                life = psc.AccumulatedHours;
+                simulationSystem.ProposedSimulationTable.Add(psc);
+            }
+            simulationSystem.ProposedPerformanceMeasures.BearingCost = simulationSystem.ProposedSimulationTable.Count * simulationSystem.BearingCost * simulationSystem.NumberOfBearings;
+            simulationSystem.ProposedPerformanceMeasures.DelayCost = delayt * simulationSystem.DowntimeCost;
+            simulationSystem.ProposedPerformanceMeasures.DowntimeCost = simulationSystem.ProposedSimulationTable.Count * simulationSystem.RepairTimeForAllBearings * simulationSystem.DowntimeCost;
+            simulationSystem.ProposedPerformanceMeasures.RepairPersonCost = (decimal)simulationSystem.ProposedSimulationTable.Count * simulationSystem.RepairTimeForAllBearings * simulationSystem.RepairPersonCost / 60;
+
+            simulationSystem.ProposedPerformanceMeasures.TotalCost = simulationSystem.ProposedPerformanceMeasures.BearingCost 
+                + simulationSystem.ProposedPerformanceMeasures.DelayCost + simulationSystem.ProposedPerformanceMeasures.DowntimeCost 
+                + simulationSystem.ProposedPerformanceMeasures.RepairPersonCost;
+
+            dataGridView2.DataSource = simulationSystem.ProposedSimulationTable;
+            for (int i = 1; i <= simulationSystem.NumberOfBearings; i++)
+                dataGridView3.Columns.Add("Bearing" + i.ToString() + "Life", "Bearing " + i.ToString() + " Life");
+            dataGridView3.RowCount = simulationSystem.ProposedSimulationTable.Count;
+            for (int i = 0; i < simulationSystem.ProposedSimulationTable.Count; i++)
+            {
+                for (int j = 0; j < simulationSystem.NumberOfBearings; j++)
+                    dataGridView3[j, i].Value = simulationSystem.ProposedSimulationTable[i].Bearings[j].Hours;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    TakeInput();
+                    BuildTable1();
+                    BuildTable2();
+                }
+                catch
+                {
+                    MessageBox.Show("You have selected a worng file!");
+                }
+            }
+        }
+    }
+}
